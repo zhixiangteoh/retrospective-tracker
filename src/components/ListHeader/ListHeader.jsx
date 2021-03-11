a99e6865e8bf1081ec8bd305cf3a1d73dfe6b1b3
Zhixiang <zhixiangteoh@gmail.com>
2021-03-11 00:07:02 -0500

    Refactor to use shards Button

--- src/components/ListHeader/ListHeader.jsx
+++ src/components/ListHeader/ListHeader.jsx
@@ -1,18 +1,30 @@
-import React, { useRef } from "react";
-import { ChevronDown } from "react-feather";
+import React from "react";
+import { Button } from "shards-react";
+import { ChevronDown, ChevronUp } from "react-feather";
 
 import getmmmddyyyy from "util/getmmmddyyyy";
 
-const ListHeader = ({ header, mondayDate, ...props }) => {
+const ListHeader = ({ header, mondayDate, collapsed, ...props }) => {
   return (
-    <div
-      className="mt-4 py-2 rounded d-flex flex-row justify-content-between align-items-center"
+    <Button
+      outline
+      block
+      theme="light"
+      style={{
+        border: 0,
+        paddingTop: 2,
+        paddingBottom: 2,
+        paddingLeft: 0,
+        paddingRight: 0,
+        fontSize: 16,
+      }}
+      className="d-flex flex-row justify-content-between align-items-center"
       {...props}
     >
       <div className="text-uppercase">{header}</div>
-      <ChevronDown size={16} />
+      {collapsed ? <ChevronDown size={16} /> : <ChevronUp size={16} />}
       <div className="text-uppercase">{`Mon ${getmmmddyyyy(mondayDate)}`}</div>
-    </div>
+    </Button>
   );
 };
 
