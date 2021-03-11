4641fe608c8b8dbad702cf612079eb8fe499bf76
Zhixiang <zhixiangteoh@gmail.com>
2021-03-11 00:08:02 -0500

    Add Save icon to Current header

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const PageHeader = ({ header }) => {
-  return <h5>{header}</h5>;
+const PageHeader = ({ header, action }) => {
+  const Action = action ? action : null;
+
+  return (
+    <div className="d-flex flex-row justify-content-between">
+      <h5>{header}</h5>
+      {action ? <Action size={20} /> : null}
+    </div>
+  );
 };
 
 export default PageHeader;
