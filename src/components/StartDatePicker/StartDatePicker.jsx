20ed96a72cb8eef0a673db3bac4d17a35568b498
saydus <saydus07@gmail.com>
2021-03-12 00:34:20 -0600

    Deleted unused code and set up boilerplate for updating state

--- src/components/StartDatePicker/StartDatePicker.jsx
+++ src/components/StartDatePicker/StartDatePicker.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
-import { Button } from "shards-react";
 import { DatePicker } from 'react-rainbow-components';
 
 
-import List from "components/List";
-import ListContainer from "components/ListContainer";
-import { ListProvider } from "context/List";
-import getMondayDate from "util/getMondayDate";
-import getddmm from "../../util/getddmm";
 
 const containerStyles = {
     maxWidth: 400,
@@ -15,18 +9,14 @@ const containerStyles = {
 
 const initialState = { startDate: undefined };
 
+const setState = ({ startDate }) => {
+    initialState.startDate = startDate;
+    // do some logic for updating the global states
+}
+
 
 const StartDatePicker = () => {
-  const [key, setKey] = useState(currentMonday);
-
-  const handleSave = () => {
-    const nextMonday = getMondayDate(
-      new Date(key.getTime() + 7 * 24 * 60 * 60 * 1000)
-    ); // Monday of next week
-    setKey(nextMonday);
-    setCurrentMonday(nextMonday);
-  };
-
+  
   return (
     <div
         className="rainbow-align-content_center rainbow-m-vertical_large rainbow-p-horizontal_small rainbow-m_auto"
@@ -36,7 +26,7 @@ const StartDatePicker = () => {
             required
             error="Select a date is Required"
             placeholder="Select a date"
-            value={state.date}
+            value={initialState.startDate}
             label="Pick the start day of the hackathon"
             onChange={value => setState({ startDate: value })}
         />
