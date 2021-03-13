ca9ea2596ca1d191d7df13c9fd0856d6fd399313
Zhixiang <zhixiangteoh@gmail.com>
2021-03-12 21:00:09 -0500

    Disable start next week if >1 week into future

--- src/components/Current/Current.jsx
+++ src/components/Current/Current.jsx
@@ -5,11 +5,31 @@ import List from "components/List";
 import ListContainer from "components/ListContainer";
 import { ListProvider } from "context/List";
 import getMondayDate from "util/getMondayDate";
+import getDayDiff from "util/getDayDiff";
 import getddmm from "../../util/getddmm";
 
-const Save = (props) => {
+const getNextMonday = (date) => {
+  const nextMonday = getMondayDate(
+    new Date(date.getTime() + 7 * 24 * 60 * 60 * 1000)
+  ); // Monday of next week
+
+  return nextMonday;
+};
+
+const getCurrentMonday = () => {
+  return getMondayDate(new Date()); // Monday of this week
+};
+
+const Save = ({ disabled, ...props }) => {
   return (
-    <Button outline pill size="sm" {...props}>
+    <Button
+      outline
+      pill
+      disabled={disabled}
+      theme={disabled ? "danger" : "primary"}
+      size="sm"
+      {...props}
+    >
       Start New Week
     </Button>
   );
@@ -23,10 +43,12 @@ const Current = ({
 }) => {
   const [key, setKey] = useState(currentMonday);
 
+  const isSaveable = () => {
+    return getDayDiff(getCurrentMonday(), getNextMonday(key)) <= 7;
+  };
+
   const handleSave = () => {
-    const nextMonday = getMondayDate(
-      new Date(key.getTime() + 7 * 24 * 60 * 60 * 1000)
-    ); // Monday of next week
+    const nextMonday = getNextMonday(key);
     setKey(nextMonday);
     setCurrentMonday(nextMonday);
   };
@@ -51,7 +73,11 @@ const Current = ({
           >
             {`${getddmm(key)} - ${getddmm(key.addDays ? key.addDays(5) : key)}`}
           </div>
-          <Save onClick={handleSave} />
+          <Save
+            onClick={isSaveable() ? handleSave : () => {}}
+            style={!isSaveable() ? { cursor: "not-allowed" } : null}
+            disabled={!isSaveable()}
+          />
         </div>
         <List refreshActions={refreshActions} />
       </ListContainer>
